8296e93600b4ad1c6d4b04879b3a63e215b0c3e2
katze <wolff.joshua2002@icloud.com>
2026-03-21 23:43:58 +0100

    feat: telescope find keybinds

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,4 +3,5 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 
+require("lua.config.keymaps")
 require("lua.config.lazy")
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,5 +1,14 @@
 return {
 		'nvim-telescope/telescope.nvim',
 		tag = '0.1.5',
-		dependencies = { 'nvim-lua/plenary.nvim' }
+		dependencies = { 'nvim-lua/plenary.nvim' },
+
+		config = function()
+				local telescope = require("telescope")
+				local builtin = require("telescope.builtin")
+
+				telescope.setup()
+
+				vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+		end,
 }
