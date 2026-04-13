0a9954f0531ca31508f27b656535aca36ff81d8e
katze <wolff.joshua2002@icloud.com>
2026-04-13 21:58:30 +0200

    fix: base autocomplete for python

--- nvim/init.lua
+++ nvim/init.lua
@@ -19,12 +19,29 @@ vim.pack.add{
 	{ src = "https://github.com/BurntSushi/ripgrep" },
 }
 
+vim.lsp.enable("pyright")
 
 
 require("config.keymaps")
 require("config.lazy")
 
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(ev)
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if client and client:supports_method("textDocument/completion") then
+			vim.opt.completeopt = { "menuone", "noselect", "popup" }
+			vim.lsp.completion.enable(true, client.id, ev.buf, {
+				autotrigger = true,
+			})
+		end
+	end,
+})
+
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = { 'python' },
 	callback = function() vim.treesitter.start() end,
 })
+
+vim.keymap.set("i", "<C-Space>", function()
+	vim.lsp.completion.get()
+end)
