26713cc87594d1255d294f8e131fec1c1151b298
katze <wolff.joshua2002@icloud.com>
2026-04-14 13:53:37 +0200

    feat: added keybind -> telescope find files opening files in vsp

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,14 +1,35 @@
 return {
-		'nvim-telescope/telescope.nvim',
-		tag = '0.1.5',
-		dependencies = { 'nvim-lua/plenary.nvim' },
+	'nvim-telescope/telescope.nvim',
+	tag = '0.1.5',
+	dependencies = { 'nvim-lua/plenary.nvim' },
 
-		config = function()
-				local telescope = require("telescope")
-				local builtin = require("telescope.builtin")
+	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
+		local builtin = require("telescope.builtin")
 
-				telescope.setup()
+		telescope.setup()
 
-				vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
-		end,
+		vim.keymap.set("n", "<leader>ff", function()
+			builtin.find_files({
+				attach_mappings = function(_, map)
+					-- Edit 
+					map("i", "<CR>", function(prompt_bufnr)
+						actions.select_default(prompt_bufnr)
+					end)
+					map("n", "<CR>", function(prompt_bufnr)
+						actions.select_default(prompt_bufnr)
+					end)
+					-- VSP 
+					map("i", ":v<CR>", function(prompt_bufnr)
+						actions.select_vertical(prompt_bufnr)
+					end)
+					map("n", ":v<CR>", function(prompt_bufnr)
+						actions.select_vertical(prompt_bufnr)
+					end)
+					return true
+				end,
+			})
+		end, { desc = "Find files (vsp)" })
+	end,
 }
