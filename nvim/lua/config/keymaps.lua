023b3beba82a29c37b0d9b332ce2c141bf5cc6c9
katze <wolff.joshua2002@icloud.com>
2026-04-18 23:13:55 +0200

    [chore]: remapped key for changing window sizes

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,9 +1,8 @@
 vim.g.mapleader = " "
-
 --
 -- Buffer Window Resize Keybinds
 --
-vim.keymap.set("n", "<C-h>", ":resize -5<CR>", { desc = "Decrease buffer height", silent = true })
-vim.keymap.set("n", "<C-j>", ":vertical resize +5<CR>", { desc = "Increase buffer width", silent = true })
-vim.keymap.set("n", "<C-k>", ":vertical resize -5<CR>", { desc = "Decrease buffer width", silent = true })
-vim.keymap.set("n", "<C-l>", ":resize +5<CR>", { desc = "Increase buffer height", silent = true })
+vim.keymap.set("n", "<C-h>", ":vertical resize -5<CR>", { desc = "Decrease buffer width", silent = true })
+vim.keymap.set("n", "<C-j>", ":resize +5<CR>", { desc = "Increase buffer height", silent = true })
+vim.keymap.set("n", "<C-k>", ":resize -5<CR>", { desc = "Decrease buffer height", silent = true })
+vim.keymap.set("n", "<C-l>", ":vertical resize +5<CR>", { desc = "Increase buffer width", silent = true })
