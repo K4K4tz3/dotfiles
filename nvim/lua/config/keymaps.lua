335bb59f6ed5ec2231ef2343b9b407d2cec1ef4b
katze <wolff.joshua2002@icloud.com>
2026-04-18 23:29:55 +0200

    [feat]: added keymaps for moving around windows

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,8 +1,16 @@
 vim.g.mapleader = " "
 --
--- Buffer Window Resize Keybinds
+-- Window Resize Keybinds
 --
-vim.keymap.set("n", "<C-h>", ":vertical resize -5<CR>", { desc = "Decrease buffer width", silent = true })
-vim.keymap.set("n", "<C-j>", ":resize +5<CR>", { desc = "Increase buffer height", silent = true })
-vim.keymap.set("n", "<C-k>", ":resize -5<CR>", { desc = "Decrease buffer height", silent = true })
-vim.keymap.set("n", "<C-l>", ":vertical resize +5<CR>", { desc = "Increase buffer width", silent = true })
+vim.keymap.set("n", "<S-h>", ":vertical resize -5<CR>", { desc = "Decrease buffer width", silent = true })
+vim.keymap.set("n", "<S-j>", ":resize +5<CR>", { desc = "Increase buffer height", silent = true })
+vim.keymap.set("n", "<S-k>", ":resize -5<CR>", { desc = "Decrease buffer height", silent = true })
+vim.keymap.set("n", "<S-l>", ":vertical resize +5<CR>", { desc = "Increase buffer width", silent = true })
+
+--
+-- Window Self - Movement
+--
+vim.keymap.set("n", "<C-h>", "<C-W>h", { desc = "Move to left window" })
+vim.keymap.set("n", "<C-j>", "<C-W>j", { desc = "Move to bottom window" })
+vim.keymap.set("n", "<C-k>", "<C-W>k", { desc = "Move to top window" })
+vim.keymap.set("n", "<C-l>", "<C-W>l", { desc = "Move to right window" })
