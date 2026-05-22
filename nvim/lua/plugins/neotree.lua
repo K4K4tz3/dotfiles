2df17618ab211fcdf8beaa4779f760d765715e62
katze <wolff.joshua2002@icloud.com>
2026-05-23 00:02:12 +0200

    [feat]: added window picking for NeoTree

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -2,11 +2,28 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
+
     dependencies = {
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
       "nvim-tree/nvim-web-devicons", -- optional, but recommended
+			"s1n7ax/nvim-window-picker",
     },
+
     lazy = false, -- neo-tree will lazily load itself
+
+		config = function()
+			require("neo-tree").setup({
+				filesystem = {
+					window = {
+						mappings = {
+							["w"] = "open_with_window_picker",
+							["S"] = "split_with_window_picker",
+							["s"] = "vsplit_with_window_picker",
+						},
+					},
+				},
+			})
+		end,
   }
 }
