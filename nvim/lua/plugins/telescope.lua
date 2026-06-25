40f0b17956cf99817a1e575309afc107fdb3d4b9
katze <wolff.joshua2002@icloud.com>
2026-06-25 22:43:19 +0200

    [feat]: finished proper find files and search files keymaps for telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -10,6 +10,9 @@ return {
 
 		telescope.setup()
 
+		--
+		-- Find files in root dir
+		--
 		vim.keymap.set("n", "<leader>ff", function()
 			builtin.find_files({
 				attach_mappings = function(_, map)
@@ -30,10 +33,60 @@ return {
 					return true
 				end,
 			})
-		end, { desc = "Find files (vsp)" })
+		end, { desc = "Find Files: Root Directory" })
 
-		vim.keymap.set("n", "<leader>ft", function()
+
+		--
+		-- Find files in buffer list
+		--
+		vim.keymap.set("n", "<leader>fb", function()
+			builtin.buffers({
+				attach_mappings = function(_, map)
+					-- Edit 
+					map("i", "<CR>", function(prompt_bufnr)
+						actions.select_default(prompt_bufnr)
+					end)
+					map("n", "<CR>", function(prompt_bufnr)
+						actions.select_default(prompt_bufnr)
+					end)
+					-- VSP 
+					map("i", ":v<CR>", function(prompt_bufnr)
+						actions.select_vertical(prompt_bufnr)
+					end)
+					map("n", ":v<CR>", function(prompt_bufnr)
+						actions.select_vertical(prompt_bufnr)
+					end)
+					return true
+				end,
+			})
+		end, { desc = "Find Files: Buffer List" })
+
+
+
+
+		--
+		-- Find string in root files
+		--
+		vim.keymap.set("n", "<leader>sr", function()
 			builtin.live_grep()
-		end, { desc = "Find inputted string in directory" })
+		end, { desc = "Search Root: Find string inside root files" })
+
+
+		--
+		-- Find string in current buffer
+		--
+		vim.keymap.set("n", "<leader>sb", function()
+			builtin.current_buffer_fuzzy_find()
+		end, { desc = "Search Buffer: Find string inside current buffer" })
+
+
+		--
+		-- Find string in current buffer
+		--
+		vim.keymap.set("n", "<leader>sa", function()
+			builtin.live_grep({
+				grep_open_files = true,
+			})
+		end, { desc = "Search Active: Find string in active buffers" })
 	end,
 }
